107db883b9aa49144d05f9798fe6aac818141e09
David Thomason <dlthomas@us.ibm.com>
2017-08-29 13:59:56 -0400

    Deployable to CloudFoundry

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 } else {
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use('/', index);
 }
 app.use('/users', users);
