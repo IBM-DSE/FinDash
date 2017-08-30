44b31a30f9e72f1e6bca0dd0888658e80c7f728a
David Thomason <dlthomas@us.ibm.com>
2017-08-30 11:49:23 -0400

    Moved StockChart into separate component can update state & chart form props

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,20 @@ import {Line} from 'react-chartjs-2';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.stock_list = this.stock_list.bind(this);
     this.toggleCheckbox = this.toggleCheckbox.bind(this);
+    this.state = {
+      users: [],
+      stocks: {
+        auto_stocks: [],
+        airline_stocks: [],
+        hotel_stocks: []
+      },
+      displayStocks: ['F']
+    };
   }
-
-  state = {
-    users: [],
-    stocks: {
-      auto_stocks: [],
-      airline_stocks: [],
-      hotel_stocks: []
-    },
-    displayStocks: ['F'],
-    chartData: {}
-  };
 
   componentDidMount() {
     fetch('/users')
@@ -27,13 +25,10 @@ class App extends Component {
     fetch('/stocks')
       .then(res => res.json())
       .then(stocks => this.setState({ stocks }));
-    fetch('/stocks/TSLA')
-      .then(res => res.json())
-      .then(stock_data => generateChartData(stock_data))
-      .then(chartData => this.setState({ chartData }));
   }
 
   render() {
+    const displayStocks = this.state.displayStocks;
     return (
       <div className="App">
         <div className="container">
@@ -41,8 +36,7 @@ class App extends Component {
             <div className="col-md-8">
 
               <h2>Stocks</h2>
-              <div>{this.state.displayStocks}</div>
-              <Line data={this.state.chartData} />
+              <StockChart displayStocks={displayStocks} />
 
               <div className="row">
                 <div className="col-md-4">
@@ -98,45 +92,69 @@ class App extends Component {
 }
 
 function list(arr, id_prefix=''){
-  console.log(id_prefix);
   return arr.map(elem =>
     <div key={id_prefix+elem.id} id={id_prefix+elem.id}>{elem.name}</div>
   )
 }
 
-function generateChartData(stock_data){
-  let chartData = data;
-  chartData.labels = stock_data.dates;
-  chartData.datasets[0].label = stock_data.stock;
-  chartData.datasets[0].data = stock_data.prices;
-  return chartData;
+class StockChart extends Component {
+  constructor(props) {
+    super(props);
+    this.generateChartData = this.generateChartData.bind(this);
+    this.state = {
+      chartData: {
+        labels: [],
+        datasets: []
+      }
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    fetch('/stocks/TSLA')
+      .then(res => res.json())
+      .then(stock_data => this.generateChartData(stock_data, this.props.displayStocks))
+      .then(chartData => this.setState({ chartData }));
+  }
+
+  render() { return (<Line data={this.state.chartData} />); }
+
+  generateChartData(stock_data, stocks){
+    let chartData = {};
+    chartData.labels = stock_data.dates;
+
+    let datasets = [];
+    for(let i in stocks){
+      let dataset = JSON.parse(orig_dataset);
+      dataset.label = stocks[i];
+      dataset.data = stock_data.prices;
+      datasets.push(dataset);
+    }
+    chartData.datasets = datasets;
+
+    return chartData;
+  }
 }
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'Stock Price',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
+const orig_dataset = JSON.stringify({
+  label: 'Stock Price',
+  fill: false,
+  lineTension: 0.1,
+  backgroundColor: 'rgba(75,192,192,0.4)',
+  borderColor: 'rgba(75,192,192,1)',
+  borderCapStyle: 'butt',
+  borderDash: [],
+  borderDashOffset: 0.0,
+  borderJoinStyle: 'miter',
+  pointBorderColor: 'rgba(75,192,192,1)',
+  pointBackgroundColor: '#fff',
+  pointBorderWidth: 1,
+  pointHoverRadius: 5,
+  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+  pointHoverBorderColor: 'rgba(220,220,220,1)',
+  pointHoverBorderWidth: 2,
+  pointRadius: 1,
+  pointHitRadius: 10,
+  data: [65, 59, 80, 81, 56, 55, 40]
+});
 
 export default App;
