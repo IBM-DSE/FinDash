8f346a1ce005329eb21b036999b8721890714866
David Thomason <dlthomas@us.ibm.com>
2017-09-06 10:52:43 -0400

    Displaying news stories in client

--- client/src/ClientPage.js
+++ client/src/ClientPage.js
@@ -6,7 +6,8 @@ class ClientPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      client_data: {}
+      client_data: {},
+      news: []
     }
   }
 
@@ -14,6 +15,10 @@ class ClientPage extends Component {
     fetch('/api/users/clients/'+this.props.match.params.clientId)
       .then(res => res.json())
       .then(client_data => this.setState({client_data}))
+      .catch((error) => { console.error(error); });
+    fetch('/api/stocks/news/F')
+      .then(res => res.json())
+      .then(news => this.setState({news}))
       .catch((error) => { console.error(error); });
   }
 
@@ -71,7 +76,13 @@ class ClientPage extends Component {
 
         <h3>Portfolio</h3>
         <hr className="solid-line"/>
-        <StockPanel stocks={stocks} topPanel={true}/>
+        <div className="col-md-8">
+          <StockPanel stocks={stocks} topPanel={true}/>
+        </div>
+        <div className="col-md-4">
+          <h3>News</h3>
+          {newsStories(this.state.news)}
+        </div>
         <br/><br/><br/><br/><br/><br/><br/><br/>
 
       </div>
@@ -99,6 +110,21 @@ function attributeCols(displayAttrs, hash=null) {
       <th key={key}>{key}</th>
     );
   }
+}
+
+function newsStories(news_data) {
+  return news_data.map((story, i) =>
+    <div key={'story-'+i} className="panel panel-default">
+      <div className="panel-heading">
+        <p className="align-left">{(new Date(story['NEWS_DATE'])).toString().slice(4, 16)}</p>
+        <h3 className="panel-title">{story['NEWS_TITLE']}</h3>
+      </div>
+      <div className="panel-body">
+        <p className="align-left">{story['NEWS_TEXT'].slice(0,200)+'...'}</p>
+      </div>
+      <div className="panel-footer"><a href={story['NEWS_URL']} target="_blank">{story['NEWS_URL']}</a></div>
+    </div>
+  );
 }
 
 const basicAttrs = [
