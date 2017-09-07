97a38ac6c129277a28162d5947702fe82540cdb9
David Thomason <dlthomas@us.ibm.com>
2017-09-07 13:42:19 -0400

    Calendar with Leo meeting & zoom URL & other appmts.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Glyphicon } from 'react-bootstrap';
-import StockPanel from './StockPanel';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 import ClientList from './ClientList';
 import ClientPage from './ClientPage';
 import './App.css';
+import './react-big-calendar.css';
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
 
 class App extends Component {
 
@@ -43,8 +49,8 @@ const Home = () => (
     <div className="row">
       <div className="col-md-8">
 
-        <h2>Stocks</h2>
-        <StockPanel/>
+        <h2>Agenda</h2>
+        <MyCalendar events={events}/>
 
       </div>
       <div className="col-md-4">
@@ -56,6 +62,56 @@ const Home = () => (
     </div>
   </div>
 );
+
+const MyCalendar = props => (
+  <div>
+    <BigCalendar
+      events={props.events}
+      step={15}
+      min={new Date(2017, 8, 26, 9, 0, 0, 0)}
+      max={new Date(2017, 8, 26, 17, 0, 0, 0)}
+      defaultDate={new Date(2017, 8, 26)}
+      defaultView='day'
+      components={{
+        event: Event
+      }}
+    />
+  </div>
+);
+
+function Event({ event }) {
+  return (
+    <span>
+      <strong>{ event.title }</strong>
+      <p>{ event.desc }</p>
+    </span>
+  )
+}
+
+const events = [
+  {
+    'title': 'Prep for Leo Rakes Meeting',
+    'start': new Date(2017, 8, 26, 9, 30, 0, 0),
+    'end': new Date(2017, 8, 26, 10, 0, 0, 0),
+  },
+  {
+    'title': 'Annual Review with Leo Rakes',
+    'start': new Date(2017, 8, 26, 11, 0, 0, 0),
+    'end': new Date(2017, 8, 26, 12, 0, 0, 0),
+    'desc': 'Zoom URL: https://zoom.us/j/7682934458'
+  },
+  {
+    'title': 'Prep for Frieda Freya Matthews Meeting',
+    'start': new Date(2017, 8, 26, 13, 30, 0, 0),
+    'end': new Date(2017, 8, 26, 14, 0, 0, 0),
+  },
+  {
+    'title': 'Annual Review with Frieda Freya Matthews',
+    'start': new Date(2017, 8, 26, 15, 0, 0, 0),
+    'end': new Date(2017, 8, 26, 16, 0, 0, 0),
+    'desc': 'Zoom URL: https://zoom.us/j/5983594782'
+  },
+];
 
 const About = () => (
   <div>
