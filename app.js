204bd15712ad537d769d1b9b72722fe1105dbd97
David Thomason <dlthomas@us.ibm.com>
2018-03-09 15:09:58 -0500

    Bumped node, npm, & CF buildpack versions. Ignoring dotenv in production Moved favicon to /public

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 let express = require('express');
 let path = require('path');
@@ -22,8 +24,7 @@ if (process.env.NODE_ENV === 'production')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -56,7 +57,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
