accb75fa50d0cc966e039dfe944f85f5cdf6425c
David Thomason <dtom90@users.noreply.github.com>
2019-03-13 16:48:57 -0400

    Client query working with camelCase modification

--- routes/clients.js
+++ routes/clients.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const db = (process.env.DB_HOST && process.env.DB_PORT &&
-            process.env.DB_USER && process.env.DB_PASS &&
-            process.env.DB_BASE) ? require('../db/ibm-db') : require('../db/sqlite-db');
+const db2 = (process.env.DB_HOST && process.env.DB_PORT &&
+             process.env.DB_USER && process.env.DB_PASS &&
+             process.env.DB_BASE);
+const db = db2 ? require('../db/ibm-db') : require('../db/sqlite-db');
 
 
 // users reference object
@@ -19,6 +20,7 @@ router.get('/', function(req, res) {
   db.queryDatabase("SELECT CustID, AccountBalance FROM BROKERAGE_CUST WHERE CustID IN (" + ids + ")",
     async (clientDetails) => {
       clientDetails = clientDetails.reduce((acc, client) => {
+        client = camelCase(client);
         acc[client.CustID] = client.AccountBalance;
         return acc;
       }, {});
@@ -36,13 +38,29 @@ router.get('/:id', async function(req, res) {
 
   const client = users['clients'].filter((client) => client.id === clientID)[0];
 
-  db.queryDatabase( "SELECT * FROM BROKERAGE_CUST WHERE \"CustID\"="+clientID,
+  db.queryDatabase( `SELECT * FROM BROKERAGE_CUST WHERE ${db2 ? 'CUSTID' : '"CustID"'}=`+clientID,
     (clientDetails) => {
-      Object.assign(client, clientDetails[0]);
+      Object.assign(client, camelCase(clientDetails[0]));
       res.json(client);
     }
   );
 });
 
+const allCaps = ['CUSTID', 'GENDER', 'AGE', 'INCOME', 'EDUCATION', 'PROFESSION', 'ACCOUNTBALANCE', 'NUMTRADESPERYR',
+  'ACCOUNTTYPE', 'TRADINGSTRATEGY', 'TRADINGSTYLE', 'TECH', 'AUTO', 'HOTEL', 'AIRLINE', 'ENERGY', 'BIOTECH', 'PHARMA',
+  'UTILITIES', 'FINANCIALS', 'STAPLES', 'INDUSTRIALS'];
+const camelCases = ['CustID','Gender','Age','Income','Education','Profession','AccountBalance','NumTradesPerYr',
+  'AccountType','TradingStrategy','TradingStyle','Tech','Auto','Hotel','Airline','Energy','Biotech','Pharma',
+  'Utilities','Financials','Staples','Industrials'];
+function camelCase(clientDetails) {
+  for (let key of Object.keys(clientDetails)) {
+    let i = allCaps.indexOf(key);
+    if(i >= 0) {
+      clientDetails[camelCases[i]] = clientDetails[key];
+      delete clientDetails[key];
+    }
+  }
+  return clientDetails;
+}
 
 module.exports = router;
